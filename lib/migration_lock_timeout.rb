c4d68de896f266030f8dc6d5040f0469bf21074e
Brad Urani <bradurani@gmail.com>
2016-07-26 23:00:06 -0700

    Correct dirs

--- lib/migration_lock_timeout.rb
+++ lib/migration_lock_timeout.rb
@@ -1,4 +1,7 @@
 require "migration_lock_timeout/version"
+require "migration_lock_timeout/lock_manager"
+require "migration_lock_timeout/config"
 
 module MigrationLockTimeout
+  ActiveRecord::Migrator.prepend(MigrationLockTimeout::LockManager)
 end
