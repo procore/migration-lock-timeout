601a522f2980df64114ac98c3a6c6f2fa60d1681
Brad Urani <bradurani@gmail.com>
2016-07-26 21:32:15 -0700

    Empty gem

---
+++ lib/migration/lock/timeout.rb
@@ -0,0 +1,9 @@
+require "migration/lock/timeout/version"
+
+module Migration
+  module Lock
+    module Timeout
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/migration/lock/timeout/version.rb
@@ -0,0 +1,7 @@
+module Migration
+  module Lock
+    module Timeout
+      VERSION = "0.1.0"
+    end
+  end
+end
