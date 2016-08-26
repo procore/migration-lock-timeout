784aa8398955f0fa8624bad4998cf9c790e4b322
Brad Urani <bradurani@gmail.com>
2016-08-25 23:49:02 -0700

    Add specs for up / down migration

--- spec/migration_lock_timeout/migration_spec.rb
+++ spec/migration_lock_timeout/migration_spec.rb
@@ -6,37 +6,70 @@ RSpec.describe ActiveRecord::Migration do
 
   describe '#migrate' do
 
-    class AddFoo < ActiveRecord::Migration
-      def change
-        create_table :foo do |t|
-          t.timestamps
-        end
-      end
-    end
-
     before(:each) do
       MigrationLockTimeout.configure do |config|
         config.default_timeout = 5
       end
     end
 
+    describe 'change migration' do
+
+      class AddFoo < ActiveRecord::Migration
+        def change
+          create_table :foo do |t|
+            t.timestamps
+          end
+        end
+      end
+
+      it 'runs migrate up with timeout' do
+        migration = AddFoo.new
+        expect(ActiveRecord::Base.connection).to receive(:execute).
+          with("SET LOCAL lock_timeout = '5s'")
+        migration.migrate(:up)
+      end
+
+      it 'does not use timeout for down migration' do
+        migration = AddFoo.new
+        expect(ActiveRecord::Base.connection).not_to receive(:execute)
+        migration.migrate(:down)
+      end
+
+      it 'allows migration to run if no default timeout set' do
+        MigrationLockTimeout.config = nil
+        migration = AddFoo.new
+        expect(ActiveRecord::Base.connection).not_to receive(:execute)
+        migration.migrate(:up)
+      end
+    end
+  end
+
+  describe 'up / down migration' do
+
+    class AddBar < ActiveRecord::Migration
+      def up
+        create_table :bar do |t|
+          t.timestamps
+        end
+      end
+
+      def down
+        drop_table :bar
+      end
+    end
+
     it 'runs migrate up with timeout' do
-      migration = AddFoo.new
+      migration = AddBar.new
       expect(ActiveRecord::Base.connection).to receive(:execute).
         with("SET LOCAL lock_timeout = '5s'")
       migration.migrate(:up)
     end
 
     it 'does not use timeout for down migration' do
-      migration = AddFoo.new
+      migration = AddBar.new
       expect(ActiveRecord::Base.connection).not_to receive(:execute)
       migration.migrate(:down)
     end
-
-    it 'allows migration to run if no default timeout set' do
-      MigrationLockTimeout.config = nil
-      migration = AddFoo.new
-      migration.migrate(:up)
-    end
   end
+
 end
